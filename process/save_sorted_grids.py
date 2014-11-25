24fa1b4d76e8d70bfd939d34711ce1b402a42300
Kirk North <kirk.w.north@gmail.com>
2014-11-25 14:42:38 -0500

    Updating grid sorting script!
    This sorting script will eventually be able to output an ASCII file
    which groups radar grids together via a time (lag) criteria, ultimately
    designed to help a user with input for doing a wind retrieval.

--- process/save_sorted_grids.py
+++ process/save_sorted_grids.py
@@ -3,9 +3,9 @@
 import os
 import argparse
 import numpy as np
-import collections
 
 from re import search
+from itertools import groupby
 from datetime import datetime
 
 
@@ -31,13 +31,26 @@ def sort_by_time(sources, stamps, suffix='cdf', verbose=False):
     return [files[idx] for idx in indices], [times[idx] for idx in indices]
 
 
-def group_by_lag(files, times, stamps, lag=120):
+def group_by_lag(files, times, stamps, lag=240, verbose=False):
     """
     """
 
-    print np.diff(times, n=1)
+    tdeltas = [time - times[0] for time in times]
+    data = [[f, td] for f, td in zip(files, tdeltas)]
+    group_key = lambda (f, td): td.total_seconds() // lag
 
-    return
+    groups = []
+    for key, group in groupby(data, key=group_key):
+        groups.append(list(group))
+
+    # Remove groups with only one radar grid
+    groups = [group for group in groups if len(group) > 1]
+
+    if verbose:
+        print ('Total number of grid groups within {} sec of '
+               'each other = {}'.format(lag, len(groups)))
+
+    return groups
 
 
 if __name__ == '__main__':
@@ -46,7 +59,7 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--sources', nargs='*', type=str, help=None)
     parser.add_argument('-id', '--stamps', nargs='*', type=str, help=None)
     parser.add_argument('-o', '--output', type=str, help=None)
-    parser.add_argument('--lag', nargs='?', type=int, const=120, default=120,
+    parser.add_argument('--lag', nargs='?', type=int, const=240, default=240,
                         help=None)
     parser.add_argument('-v', '--verbose', nargs='?', type=bool, const=True,
                         default=False, help=None)
@@ -60,4 +73,4 @@ if __name__ == '__main__':
                                 verbose=args.verbose)
 
     # Group files by lag
-    group_by_lag(files, times, args.stamps, lag=args.lag)
+    group_by_lag(files, times, args.stamps, lag=args.lag, verbose=args.verbose)
