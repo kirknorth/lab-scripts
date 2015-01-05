62fca575ea4c09823093bd21afaaeafd3374471b
Kirk North <kirk.w.north@gmail.com>
2015-01-05 15:39:30 -0500

    MOD: X-band gridding script
    Almost done creating the X-band MMCG processing script!

--- process/process_xsaprinnermmcg.py
+++ process/process_xsaprinnermmcg.py
@@ -27,6 +27,17 @@ COORDS = [np.arange(0, 10250, 250),
           np.arange(-50000, 50500, 500)]
 ORIGIN = [36.605, -97.485]
 
+# Define gridding parameters
+NUM_POINTS = 250
+TOA = 17000.0
+FUNCTION = 'Barnes'
+SMOOTH = 'constant'
+CUTOFF = 5000.0
+SPACING = 1220.0
+KAPPA = 0.5
+ROI = None
+MIN_RADIUS = 250.0
+
 
 def parse_datastreams(files, version='-9999'):
     """
@@ -88,13 +99,7 @@ def create_metadata(radar, files):
     return metadata
 
 
-def process(fcmac, grid_coords, Fn, dl, facility_id, grid_origin=None,
-            fields=None, toa=17000.0, leafsize=10, k=100, eps=0.0,
-            weighting_function='Barnes', smooth_func='constant',
-            roi_func='constant', cutoff_radius=5000.0, data_spacing=1220.0,
-            kappa_star=0.5, h_factor=3.0, nb=1.5, bsp=2.0, min_radius=250.0,
-            map_roi=True, map_dist=True, proj='lcc', datum='NAD83',
-            ellps='GRS80', format='NETCDF4_CLASSIC'):
+def process(fcmac, Fn, dl, facility_id):
     """
     """
 
@@ -103,13 +108,12 @@ def process(fcmac, grid_coords, Fn, dl, facility_id, grid_origin=None,
 
     # Grid radar data
     grid = map_radar_to_grid(
-        radar, grid_coords, grid_origin=grid_origin, fields=fields, toa=toa,
-        leafsize=leafsize, k=k, eps=eps, weighting_function=weighting_function,
-        smooth_func=smooth_func, roi_func=roi_func,
-        cutoff_radius=cutoff_radius, data_spacing=data_spacing,
-        kappa_star=kappa_star, h_factor=h_factor, nb=nb, bsp=bsp,
-        min_radius=min_radius, map_roi=map_roi, map_dist=map_dist, proj=proj,
-        datum=datum, ellps=ellps, debug=False)
+        radar, COORDS, grid_origin=ORIGIN, fields=FIELDS, toa=TOA,
+        leafsize=10, k=NUM_POINTS, eps=0.0, weighting_function=FUNCTION,
+        smooth_func=SMOOTH, roi_func=ROI, cutoff_radius=CUTOFF,
+        data_spacing=SPACING, kappa_star=KAPPA, h_factor=None, nb=None,
+        bsp=None, min_radius=MIN_RADIUS, map_roi=True, map_dist=True,
+        proj='lcc', datum='NAD83', ellps='GRS80', debug=False)
 
     # ARM file name protocols
     date_stamp = num2date(grid.axes['time_start']['data'][0],
@@ -118,7 +122,7 @@ def process(fcmac, grid_coords, Fn, dl, facility_id, grid_origin=None,
         Fn, dl, date_stamp.strftime('%Y%m%d.%H%M%S'))
 
     # Write gridded data to file
-    grid.write(output + filename, format=format)
+    grid.write(os.path.join(output, filename), format=format)
 
     return
 
@@ -126,7 +130,7 @@ def process(fcmac, grid_coords, Fn, dl, facility_id, grid_origin=None,
 if __name__ = '__main__':
 
     # Parse command line arguments
-    parser = argparse.ArgumentParser(description=description)
+    parser = argparse.ArgumentParser(description=None)
     parser.add_argument('source', type=str, help=None)
     parser.add_argument('output', type=str, help=None)
     parser.add_argument('stamp', type=str, help=None)
@@ -137,8 +141,9 @@ if __name__ = '__main__':
                         help=None)
     args = parser.parse_args()
 
-    files = [args.source + f for f in sorted(os.listdir(args.source)) if
-             args.stamp in f]
+    # Parse all files to process
+    files = [os.path.join(args.source, f) for f in
+             sorted(os.listdir(args.source)) if args.stamp in f]
     if args.verbose:
         print 'Number of files to process = %i' % len(files)
 
@@ -146,5 +151,4 @@ if __name__ = '__main__':
         if args.verbose:
             print 'Processing file %s' % fcmac
 
-        process(fcmac, grid_coords, args.Fn, args.dl, args.facility_id,
-                grid_origin=grid_origin, fields=fields)
+        process(fcmac, args.Fn, args.dl, args.facility_id)
