0b4d882ec5f4ba53b04e897406c9d42114cb0d3a
Kirk North <kirk.w.north@gmail.com>
2015-04-24 18:39:53 -0400

    WIP: WSR-88D CMAC improvements
    Further improvements to processing the "raw" WSR-88D data, including
    better handling of metadata, and improvements (hopefully) to the Doppler
    velocity correction.

--- process/proc_wsr88d.a0.py
+++ process/proc_wsr88d.a0.py
@@ -13,7 +13,7 @@ from pyart.io import write_cfradial
 from pyart.aux_io import read_radx
 from pyart.util.datetime_utils import datetime_from_radar
 from pyart.correct import dealias_region_based, GateFilter
-from pyart.config import get_fillvalue, get_field_name
+from pyart.config import get_fillvalue, get_field_name, FileMetadata
 
 
 # Radar descriptors
@@ -25,19 +25,15 @@ FACILITY = 'KVNX: Vance AFB, Oklahoma'
 # Define number of sweeps for VCP
 NSWEEPS = 14
 
-# Doppler velocity correction routine
+# Doppler velocity correction routine and parameters
 DEALIAS = 'region'
+INTERVAL_SPLITS = 5
 
 # Define output NetCDF format
 FORMAT = 'NETCDF4'
 
 # Define radar field names
-REFL_FIELD = 'REF'
-VDOP_FIELD = 'VEL'
-WIDTH_FIELD = 'SW'
-ZDR_FIELD = 'ZDR'
-RHOHV_FIELD = 'RHO'
-PHIDP_FIELD = 'PHI'
+REFL_FIELD = get_field_name('reflectivity')
 CORR_REFL_FIELD = get_field_name('corrected_reflectivity')
 CORR_VDOP_FIELD = get_field_name('corrected_velocity')
 
@@ -60,39 +56,53 @@ def process_file(filename, outdir, debug=False, verbose=False):
     if NSWEEPS is not None and radar.nsweeps != NSWEEPS:
         return
 
+    if verbose:
+        print 'Processing file: {}'.format(os.path.basename(filename))
+
     # Create gate filter
     gf = GateFilter(radar, exclude_based=True)
 
-    # Step 1: Doppler velocity correction
+    # Create file metadata object
+    meta = FileMetadata(
+        'nexrad_archive', field_names=None, additional_metadata=None,
+        file_field_names=False, exclude_fields=None)
+
+    # Step 1: Remove unnecessary fields
+    for field in REMOVE_FIELDS:
+        radar.fields.pop(field, None)
+
+    # Step 2: Rename fields
+    for field in radar.fields.keys():
+        default_field = meta.get_field_name(field)
+        radar.fields[default_field] = radar.fields.pop(field, None)
+
+    # Step 3: Doppler velocity correction
     if DEALIAS == 'phase':
         vdop_corr = dealias_unwrap_phase(
             radar, gatefilter=gf, unwrap_unit='sweep', nyquist_vel=None,
-            rays_wrap_around=True, keep_original=False, vel_field=VDOP_FIELD,
+            rays_wrap_around=True, keep_original=False, vel_field=None,
             corr_vel_field=CORR_VDOP_FIELD)
 
     elif DEALIAS == 'region':
         vdop_corr = dealias_region_based(
-            radar, gatefilter=gf, interval_splits=3, interval_limits=None,
-            skip_between_rays=2, skip_along_ray=2, centered=True,
-            nyquist_vel=None, rays_wrap_around=False, keep_original=False,
-            vel_field=VDOP_FIELD, corr_vel_field=CORR_VDOP_FIELD)
+            radar, gatefilter=gf, interval_splits=INTERVAL_SPLITS,
+            interval_limits=None, skip_between_rays=2, skip_along_ray=2,
+            centered=True, nyquist_vel=None, rays_wrap_around=False,
+            keep_original=False, vel_field=None,
+            corr_vel_field=CORR_VDOP_FIELD)
     else:
         raise ValueError('Unsupported velocity correction routine')
 
     radar.add_field(CORR_VDOP_FIELD, vdop_corr, replace_existing=True)
 
-    # Step 2: Reflectivity correction
+    # Step 4: Reflectivity correction
     # Currently no correction procedures are applied to the reflectivity field
     # due to minimal attenuation at S-band
     refl_corr = radar.fields[REFL_FIELD]
     radar.add_field(CORR_REFL_FIELD, refl_corr, replace_existing=True)
 
-    # Step 3: Remove unnecessary fields
-    for field in REMOVE_FIELDS:
-        radar.fields.pop(field, None)
-
-    # Parse metadata
-    radar.metadata = _create_metadata(radar, filename)
+    # Add metadata
+    _add_metadata(radar, filename)
 
     # ARM file name protocols
     date_stamp = num2date(radar.time['data'].min(), radar.time['units'])
@@ -106,7 +116,7 @@ def process_file(filename, outdir, debug=False, verbose=False):
     return
 
 
-def _create_metadata(radar, filename):
+def _add_metadata(radar, filename):
     """
     """
 
@@ -122,34 +132,33 @@ def _create_metadata(radar, filename):
     time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     history = 'created by user {} on {} at {}'.format(user, host, time)
 
-    metadata = {
-        'process_version': '',
-        'command_line': '',
-        'site_id': '',
-        'facility_id': FACILITY,
-        'country': 'USA',
-        'project': '',
-        'institution': 'McGill University',
-        'dod_version': '',
-        'comment': '',
-        'instrument_name': radar.metadata['instrument_name'],
-        'state': '',
-        'Conventions': 'CF/Radial',
-        'reference': '',
-        'source': radar.metadata['source'],
-        'platform_type': radar.metadata['platform_type'],
-        'scan_id': radar.metadata['scan_id'],
-        'scan_name': radar.metadata['scan_name'],
-        'input_datastreams_num': 1,
-        'input_datastreams': datastream,
-        'input_datastreams_description': datastream_description,
-        'description': '',
-        'title': 'Corrected Moments in Antenna Coordinates',
-        'field_names': '',
-        'history': history,
-    }
+    # Add new metadata
+    radar.metadata['process_version'] = '',
+    radar.metadata['command_line'] = '',
+    radar.metadata['site_id'] = '',
+    radar.metadata['facility_id'] = FACILITY,
+    radar.metadata['country'] = 'USA',
+    radar.metadata['project'] = '',
+    radar.metadata['institution'] = 'McGill University',
+    radar.metadata['dod_version'] = '',
+    radar.metadata['comment'] = '',
+    radar.metadata['instrument_name'] = radar.metadata['instrument_name'],
+    radar.metadata['state'] = '',
+    radar.metadata['Conventions'] = 'CF/Radial',
+    radar.metadata['reference'] = '',
+    radar.metadata['source'] = radar.metadata['source'],
+    radar.metadata['platform_type'] = radar.metadata['platform_type'],
+    radar.metadata['scan_id'] = radar.metadata['scan_id'],
+    radar.metadata['scan_name'] = radar.metadata['scan_name'],
+    radar.metadata['input_datastreams_num'] = 1,
+    radar.metadata['input_datastreams'] = datastream,
+    radar.metadata['input_datastreams_description'] = datastream_description,
+    radar.metadata['description'] = '',
+    radar.metadata['title'] = 'Corrected Moments in Antenna Coordinates',
+    radar.metadata['field_names'] = '',
+    radar.metadata['history'] = history,
 
-    return metadata
+    return
 
 
 def _parse_datastreams(filename, dl='a0', version='-9999'):
@@ -177,11 +186,13 @@ if __name__ == '__main__':
                         const=True, help=None)
     args = parser.parse_args()
 
-    if args.debug:
+    if args.verbose:
         print 'QF -> {}'.format(QF)
         print 'FN -> {}'.format(FN)
+        print 'DL -> {}'.format(DL)
         print 'FACILITY -> {}'.format(FACILITY)
         print 'DEALIAS -> {}'.format(DEALIAS)
+        print 'INTERVAL_SPLITS -> {}'.format(INTERVAL_SPLITS)
 
     # Parse all radar files to process
     files = [os.path.join(args.inpdir, f) for f in
